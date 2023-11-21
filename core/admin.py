46ed7e711b3f7ff5795d10e256625a0d532c1a76
Diego Gomez <diegocor4@gmail.com>
2023-11-20 21:58:57 -0300

    login-permisos

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Producto, Categoria
+from .models import Producto, Categoria, Usuario, Pedido, DetallePedido
 
 # Register your models here.
 # Registro de modelos para usarlos en el Admin
 admin.site.register(Producto)
 admin.site.register(Categoria)
+admin.site.register(Usuario)
+admin.site.register(Pedido)
+admin.site.register(DetallePedido)
